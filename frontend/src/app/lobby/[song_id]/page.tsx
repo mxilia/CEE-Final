b77a66fe8d18a000710b9f26ebb7a81186322ee6
mxilia <ravintkosol@gmail.com>
2026-05-08 14:12:07 +0700

    feat: add karaoke mockdata

--- frontend/src/app/lobby/[song_id]/page.tsx
+++ frontend/src/app/lobby/[song_id]/page.tsx
@@ -2,6 +2,7 @@
 
 /* Authen ว่าเล่นได้ป่าว ในหน้านี้  */
 export default function LobbyPage({ params }: { params: { song_id: string } }) {
+    
     return (
         <div>
             <h1>Lobby</h1>
