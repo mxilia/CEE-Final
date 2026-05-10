97a73763b00f25c245e564efaaafad059fac2ff8
mxilia <ravintkosol@gmail.com>
2026-05-10 16:35:27 +0700

    fix: fix home page render

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,13 +1,16 @@
 
 import SearchBar from "@/src/components/home/search-bar";
 import SongList from "@/src/components/home/song-list";
+import { Suspense } from "react";
 
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-6 m-12">
-      <SearchBar />
-      <SongList />
+      <Suspense>
+        <SearchBar />
+        <SongList />
+      </Suspense>
     </div>
   );
 }
